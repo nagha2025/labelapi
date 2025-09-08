92aa8ec218d1e53b7fa86a6c1d01a6a933dfc9fe
Nagharajan G T <nagharajan@makersclub.in>
2025-09-08 21:06:43 +0100

    updated CORS port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3003/',
     credentials: true,
     optionSuccessStatus: 200
 }
@@ -15,7 +15,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Origin', "http://localhost:3003");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST');
