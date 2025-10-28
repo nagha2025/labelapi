70a73db4834d02057e62a0797aca1459da3bcc34
Nagharajan G T <nagharajan@makersclub.in>
2025-10-28 11:28:07 +0000

    uncommented changes to be committed:

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:3003',
-    // origin: 'https://label-app-ay54.onrender.com',
+    // origin: 'http://localhost:3003',
+    origin: 'https://label-app-ay54.onrender.com',
     credentials: true,
     optionSuccessStatus: 200
 }
@@ -16,8 +16,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3003");
-    // res.header('Access-Control-Allow-Origin', "https://label-app-ay54.onrender.com");
+    // res.header('Access-Control-Allow-Origin', "http://localhost:3003");
+    res.header('Access-Control-Allow-Origin', "https://label-app-ay54.onrender.com");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST');
