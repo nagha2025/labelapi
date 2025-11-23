2015d7c0ae251e4cca60939653d71e306039202c
Nagharajan G T <nagharajan@makersclub.in>
2025-11-23 15:32:46 +0000

    allowed all origins to access the resources.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
     // res.header('Access-Control-Allow-Origin', "http://localhost:3003");
-    res.header('Access-Control-Allow-Origin', "https://label-app-ay54.onrender.com");
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST');
